1e983434e4b1fefabe66ea708adb91d5193be7d3
Vladislav Bezrukov <106032276+VladislavBezrukov@users.noreply.github.com>
2022-08-10 19:09:07 +0300

    Update SQRService.java

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,10 +4,11 @@ public class SQRService {
 
     public int calculate(int start, int end) {
         int count = 0;
-        for (double i = start; i <= end; i++) {
-            double result = Math.sqrt(i);
-            if (result % 1 == 0) {
+        for (double i = 10; i <= 99; i++) {
+            if (i * i >= start) {
+                if(i * i <= end) {
                 count++;
+                }
             }
         }
         return count;
